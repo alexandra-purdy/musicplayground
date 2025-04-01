5b9337c1f8df4cba33de58a3418dd645d9a76ccb
alexandra-purdy <alexandrajanepurdy@gmail.com>
2025-04-01 14:40:14 -0400

    updated opening text

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ window.addEventListener("DOMContentLoaded", function () {
   
     // Check screen width and set popover content with line breaks
     if (window.innerWidth <= 500) {
-      popover.innerHTML = "welcome! click me to begin. use WASD keys to move, arrow keys to pan, and the space bar to jump. also... turn up your volume!";
+      popover.innerHTML = "welcome! click me to begin. also... turn up your volume! (and put your ringer on :) )";
     } else {
-      popover.innerHTML = "welcome! click me to begin";
+      popover.innerHTML = "welcome! click me to begin. use WASD keys to move, arrow keys to pan, and the space bar to jump. also... turn up your volume!";
     }
   
     popover.showPopover();
