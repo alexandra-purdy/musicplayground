6337900e7465f2b3ca50f14bfbfcc29773a62462
alexandra-purdy <alexandrajanepurdy@gmail.com>
2025-04-25 15:52:05 -0400

    button

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-window.addEventListener("DOMContentLoaded", function () {
-    const popover = document.getElementById("mypopover");
-  
-    // Check screen width and set popover content with line breaks
-    if (window.innerWidth <= 500) {
-      popover.innerHTML = "welcome! click me to begin. also... turn up your volume! (and put your ringer on :) )";
-    } else {
-      popover.innerHTML = "welcome! click me to begin. use WASD keys to move, arrow keys to pan, and the space bar to jump. also... turn up your volume!";
-    }
-  //push test
-    popover.showPopover();
-  
-    // Hide the popover when clicked
-    popover.addEventListener("click", function () {
-      this.hidePopover();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // POPUP LOGIC
+  const popover = document.getElementById("mypopover");
+
+  if (window.innerWidth <= 500) {
+    popover.innerHTML =
+      "welcome! click me to begin. also... turn up your volume! (and put your ringer on :) )";
+  } else {
+    popover.innerHTML =
+      "welcome! click me to begin. use WASD keys to move, arrow keys to pan, and the space bar to jump. also... turn up your volume!";
+  }
+
+  popover.showPopover();
+
+  popover.addEventListener("click", function () {
+    this.hidePopover();
   });
 
-// button  
-document.addEventListener('DOMContentLoaded', function () {
-  const restartButton = document.getElementById('restartButton');
-  restartButton.addEventListener('click', function () {
+  // RESTART BUTTON LOGIC
+  const restartButton = document.getElementById("restartButton");
+  restartButton.addEventListener("click", function () {
     location.reload();
   });
 });
-  
-  
